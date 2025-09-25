1958ace7cc282cfe24e4f773326e289f4dcfa0bf
Paolo <paoloalexis.ellevera@g.msuiit.edu.ph>
2025-09-25 14:00:55 +0800

    Add a new update for the fiftyone_start.py

--- fiftyone_start.py
+++ fiftyone_start.py
@@ -11,5 +11,14 @@ def main():
     except KeyboardInterrupt:
         print("\nSession stopped.")
 
+    session.view = dataset.sort_by("uniqueness").limit(10)
+
+    ##CREATING A GROUP
+    # dataset = foz.load_zoo_dataset("quickstart-groups")
+    # group_id = dataset.last().group.id
+
+    # session = fo.launch_app(dataset, group_id=group_id)
+
+    
 if __name__ == "__main__":
     main()
